9775f37a6e2ab22244a01746750e696fcbc4df42
Standard <standard@HHTV-2.local>
2014-07-02 12:15:02 +0700

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Standard on 7/2/14.
+//  Copyright (c) 2014 Standard Application. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Standard on 7/2/14.
+//  Copyright (c) 2014 Standard Application. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
